e2cb2006c614fce0f6d623def5f13174583233d3
Stephen Fluin <stephen.fluin@gmail.com>
2016-08-25 10:18:57 -0700

    Extract and translate strings, compile fr locale and translations

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,20 @@
 import "./polyfills.ts";
 
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { enableProdMode } from '@angular/core';
+import { enableProdMode, TRANSLATIONS, TRANSLATIONS_FORMAT, LOCALE_ID } from '@angular/core';
 import { environment } from './environments/environment';
 import { AppModule } from './app/';
+import { TRANSLATION } from './messages.fr';
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule);
+// Compile using french translations
+platformBrowserDynamic().bootstrapModule(
+  AppModule,
+  {providers: [
+    {provide: TRANSLATIONS, useValue: TRANSLATION},
+    {provide:TRANSLATIONS_FORMAT, useValue:'xlf'},
+    {provide:LOCALE_ID, useValue:'fr'}
+  ]});
---
+++ src/messages.fr.ts
@@ -0,0 +1,34 @@
+export const TRANSLATION = `
+<?xml version='1.0' encoding='utf-8'?>
+<xliff xmlns="urn:oasis:names:tc:xliff:document:1.2" version="1.2">
+ <file source-language="en" datatype="plaintext" original="ng2.template">
+   <body>
+     <trans-unit id="a05c3a315a6ee3e6ad4733a1f72e8d16775810ca" datatype="html" approved="yes">
+       <source>Hello i18n</source> <target state="translated">Bonjour i18n</target> <note priority="1" from="description">an introduction header for this sample</note>
+     </trans-unit>
+     <trans-unit id="330f03dd7f1beb59f825b8a1e19aa3b4c363c135" datatype="html" approved="yes">
+       <source>This is a sample of internationalization in Angular2.</source> <target state="translated">Ceci est un exemple d'internationalisation avec Angular2.</target> <note priority="1" from="description">a paragraph explaining what this sample is</note>
+     </trans-unit>
+     <trans-unit id="2e9f59ccc95e6f62d9b746d04a12d892de43c7e6" datatype="html" approved="yes">
+       <source>Attribute sample</source> <target state="translated">Exemple d'attribut</target> </trans-unit>
+     <trans-unit id="533b2b9a76ee1335cb44c01f0bfd50d43e9400b0" datatype="html" approved="yes">
+       <source>Your name</source> <target state="translated">Votre nom</target> <note priority="1" from="description">a placeholder for field collecting the user's name</note>
+     </trans-unit>
+     <trans-unit id="3a64caf216dac5b830519cddbf59421c3a4730cf" datatype="html" approved="yes">
+       <source>Multiple Element Sample</source> <target state="translated">Exemple avec plusier éléments</target> </trans-unit>
+     <trans-unit id="a38f08018348aefa90b876185b02cd3baf2a5e8c" datatype="html" approved="yes">
+       <source><x id="START_PARAGRAPH" ctype="p"/>Element 1<x id="CLOSE_PARAGRAPH" ctype="p"/> <x id="START_PARAGRAPH" ctype="p"/>Element 2<x id="CLOSE_PARAGRAPH" ctype="p"/> </source> 
+       <target state="translated"><x id="START_PARAGRAPH" ctype="p"/>Elément 1<x id="CLOSE_PARAGRAPH" ctype="p"/> <x id="START_PARAGRAPH" ctype="p"/>Elément 2<x id="CLOSE_PARAGRAPH" ctype="p"/> </target> <note priority="1" from="description">multiple element examples</note>
+     </trans-unit>
+     <trans-unit id="3782a1b759c918ef77cd145ef9ac974de4c3f18a" datatype="html" approved="yes">
+       <source>Meaning Sample</source> <target state="translated">Exemple de sens</target> </trans-unit>
+     <trans-unit id="cb34c494bf21eb1f8b8ceeccb1a71a5f8ebb1a5c" datatype="html" approved="yes">
+       <source>Hello <x id="INTERPOLATION"/></source> <target state="translated">Bonjour <x id="INTERPOLATION"/></target> <note priority="1" from="description">a message welcoming the user by name</note>
+       <note priority="1" from="meaning">user welcome</note>
+     </trans-unit>
+   </body>
+ </file>
+</xliff>
+
+
+`;
